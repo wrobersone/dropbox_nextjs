1987f77050ec9a981af27ce08a1042e9a4a6729c
William Robersone <william.robersone@gmail.com>
2024-02-01 00:53:30 -0600

    home page

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import React from 'react'
+import { ThemeToggle } from "./ThemeToggle"
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="bg-[#0160FE] w-fit">
+          <Image 
+            src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
+            alt="logo"
+            className="invert"
+            height={50}
+            width={50}
+          />
+        </div>
+        <h1 className="font-bold text-xl">Dropbox</h1>
+      </Link>
+
+    {/* Clerk Theme Toggler */}
+      <div className="flex items-center px-5 space-x-2">
+        <ThemeToggle />
+
+        <UserButton afterSignOutUrl="/" />
+
+        <SignedOut>
+          <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+        </SignedOut>
+      </div>
+    </header>
+  )
+}
+
+export default Header
